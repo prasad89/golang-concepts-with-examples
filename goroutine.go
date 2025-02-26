20ffbe83469649eee2967e4de77610f7d917fd98
Prasad Bhalerao <67261499+prasad89@users.noreply.github.com>
2025-02-26 11:05:28 +0000

    add goroutine example to demonstrate concurrent number printing

---
+++ goroutine.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printNumber(n int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("The number is:", n)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go printNumber(i, &wg)
+	}
+
+	wg.Wait()
+}
